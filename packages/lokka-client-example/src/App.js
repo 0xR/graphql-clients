d49833fbf572e4d49897b6cf91ea6290c261f8b7
Ruben Oostinga <roostinga@xebia.com>
2016-11-13 21:37:46 +0100

    Add redux in single file

--- packages/lokka-client-example/src/App.js
+++ packages/lokka-client-example/src/App.js
@@ -1,6 +1,62 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { createStore } from 'redux';
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
+const mapStateToProps = (state) => {
+  return {
+    value: state
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrement: () => {
+      dispatch({
+        type: 'INCREMENT'
+      })
+    },
+    onDecrement: () => {
+      dispatch({
+        type: 'DECREMENT'
+      })
+    }
+  };
+};
+
+const CounterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+/*
+ * Let's create a store.
+ */
+const store = createStore(counter);
 
 class App extends Component {
   render() {
@@ -13,6 +69,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <CounterContainer store={store} />,
       </div>
     );
   }
