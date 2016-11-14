e3d48d8d232ca9a59ab99f74b890b1c01fad7eb9
Ruben Oostinga <roostinga@xebia.com>
2016-11-14 16:02:09 +0100

    Add apollo pagination

--- packages/apollo-client-example/src/App.js
+++ packages/apollo-client-example/src/App.js
@@ -1,16 +1,17 @@
+/* eslint-disable react/prop-types,react/jsx-filename-extension */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { connect } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import Lokka from 'lokka';
-import Transport from 'lokka-transport-http';
 import thunk from 'redux-thunk';
 import { createActions, handleActions } from 'redux-actions';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider, graphql } from 'react-apollo';
+import { connect } from 'react-redux';
+import gql from 'graphql-tag';
 
-const client = new Lokka({
-  transport: new Transport('/graphql'),
-});
+import logo from './logo.svg';
+import './App.css';
+
+const client = new ApolloClient();
 
 const actions = createActions('UPDATE_PAGE', 'POSTS', 'POST_ERRORS');
 
@@ -18,101 +19,73 @@ const currentPageNumberReducer = handleActions({
   [actions.updatePage]: (state, { payload }) => state + payload,
 }, 1);
 
-const postsByPageReducer = handleActions({
-  [actions.posts]: (
-    state,
-    { payload: { page, response } }
-  ) => ({
-    ...state,
-    [page]: response,
-  }),
-}, {});
-
-const errorsReducer = handleActions({
-  [actions.postErrors]: (state, { payload }) => payload,
-  [actions.posts]: () => [],
-}, []);
-
-
 const rootReducer = combineReducers({
   currentPageNumber: currentPageNumberReducer,
-  postsByPage: postsByPageReducer,
-  errors: errorsReducer,
+  apollo: client.reducer(),
 });
 
 const Posts = ({
-  value,
-  errors,
-  posts,
+  data: {
+    authors: [{ posts }] = [{}],
+    loading,
+    error: { graphQLErrors, networkError } = {},
+  },
+  pageNumber,
   onIncrement,
   onDecrement,
 }) => (
   <div>
-    <h1>{value}</h1>
+    <h1>{pageNumber}</h1>
     <button onClick={onIncrement}>+</button>
     <button onClick={onDecrement}>-</button>
-    {errors.map(error => <pre key={error} >{error}</pre>)}
-    {posts.map(({ title, id }) => <p key={id} >{title}</p>)}
+    <p>{loading ? 'loading...' : null}</p>
+    {networkError && <pre>{networkError.message}</pre>}
+    {graphQLErrors && graphQLErrors.map(({ message }) => <pre key={message} >{message}</pre>)}
+    {posts && posts.map(({ title, id }) => <p key={id} >{title}</p>)}
   </div>
 );
 
 const mapStateToProps = state => ({
-  value: state.currentPageNumber,
-  errors: state.errors,
-  posts: state.postsByPage[state.currentPageNumber] || [],
+  pageNumber: state.currentPageNumber,
 });
 
 const pageSize = 5;
 
-function fetchPosts(pageOffset) {
-  return (dispatch, getState) => {
-    dispatch(actions.updatePage(pageOffset));
-
-    const { currentPageNumber, postsByPage } = getState();
-
-    const posts = postsByPage[currentPageNumber];
-
-    if (!posts) {
-      const offset = (currentPageNumber - 1) * pageSize;
-
-      client.query(`query getPosts($offset: Int, $limit: Int){
-        authors(limit:1){
-          firstName
-          lastName,
-          posts(offset: $offset, limit: $limit){
-            id
-            title
-            text
-          }
-        }
-      }`, { offset, limit: pageSize }).then(
-        ({ authors: [{ posts: response }] }) => dispatch(actions.posts({ page: currentPageNumber, response })),
-        (error) => {
-          const { rawError } = error;
-          if (rawError) {
-            dispatch(actions.postErrors(rawError.map(({ message }) => message)));
-          } else {
-            dispatch(actions.postErrors([error.message]));
-          }
-        },
-      );
-    }
-  };
-}
-
 const mapDispatchToProps = dispatch => ({
   onIncrement() {
-    dispatch(fetchPosts(1));
+    dispatch(actions.updatePage(1));
   },
   onDecrement() {
-    dispatch(fetchPosts(-1));
+    dispatch(actions.updatePage(-1));
   },
 });
 
-const PostsContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps,
+
+const postQuery = gql`query getPosts($offset: Int, $limit: Int){
+  authors(limit:1){
+    firstName
+    lastName,
+    posts(offset: $offset, limit: $limit){
+      id
+      title
+      text
+    }
+  }
+}`;
+
+const PostsContainer = compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  graphql(
+    postQuery, {
+      options: ({ pageNumber }) => ({
+        variables: {
+          offset: (pageNumber - 1) * pageSize,
+          limit: pageSize,
+        },
+      }),
+    }),
 )(Posts);
+
 
 const debugStoreEnhancer = (
   typeof window === 'object' && window.devToolsExtension
@@ -125,8 +98,6 @@ const enhancers = compose(
 
 const store = createStore(rootReducer, enhancers);
 
-store.dispatch(fetchPosts(0));
-
 class App extends Component {
   render() {
     return (
@@ -138,10 +109,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <PostsContainer store={store} />
+        <PostsContainer />
       </div>
     );
   }
 }
 
-export default App;
+export default () => (
+  <ApolloProvider store={store} client={client}>
+    <App />
+  </ApolloProvider>
+);
